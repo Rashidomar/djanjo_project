6acd5d16807538e631cc5a82ae3107ab3b484c2c
Rashid Omar <amidurashid@gmail.com>
2019-03-12 14:48:53 +0000

    I mode some change I delete profile.html file I added dashboard.html to the project I changes to the index.html, accounts.views and accounts.urls file I have done some changes to the index by adding styles to it

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'posts',
     'accounts',
     'django.contrib.admin',
@@ -128,9 +129,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+
